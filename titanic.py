a2dec97741ff8928a64881b53ec77d5d9f66fc33
faiz5190 <97604215+faiz5190@users.noreply.github.com>
2022-04-02 14:18:06 +0800

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -1,68 +1,234 @@
-import numpy as np
+import streamlit as st 
+import numpy as np 
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+
+#import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
+
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
-data = pd.read_csv('500_Person_Gender_Height_Weight_Index.csv')
-print(data.describe())
-def give_names_to_indices(ind):
-    if ind==0:
-        return 'Extremely Weak'
-    elif ind==1:
-        return 'Weak'
-    elif ind==2:
-        return 'Normal'
-    elif ind==3:
-        return 'OverWeight'
-    elif ind==4:
-        return 'Obesity'
-    elif ind==5:
-        return 'Extremely Obese'
-data['Index'] = data['Index'].apply(give_names_to_indices)
-sns.lmplot('Height','Weight',data,hue='Index',size=7,aspect=1,fit_reg=False)
-people = data['Gender'].value_counts()
-categories = data['Index'].value_counts()
-# STATS FOR MEN
-data[data['Gender']=='Male']['Index'].value_counts()
-# STATS FOR WOMEN
-data[data['Gender']=='Female']['Index'].value_counts()
-data2 = pd.get_dummies(data['Gender'])
-data.drop('Gender',axis=1,inplace=True)
-data = pd.concat([data,data2],axis=1)
-y=data['Index']
-data =data.drop(['Index'],axis=1)
-scaler = StandardScaler()
-data = scaler.fit_transform(data)
-data=pd.DataFrame(data)
-X_train, X_test, y_train, y_test = train_test_split(data, y, test_size=0.3, random_state=101)
-param_grid = {'n_estimators':[100,200,300,400,500,600,700,800,1000]}
-grid_cv = GridSearchCV(RandomForestClassifier(random_state=101),param_grid,verbose=3)
-grid_cv.fit(X_train,y_train)
-print(grid_cv.best_params_)
-# weight category prediction
-pred = grid_cv.predict(X_test)
-print(classification_report(y_test,pred))
-print('\n')
-print(confusion_matrix(y_test,pred))
-print('\n')
-print('Acuuracy is --> ',accuracy_score(y_test,pred)*100)
-print('\n')
-def lp(details):
-    gender = details[0]
-    height = details[1]
-    weight = details[2]
+
+from sklearn.metrics import classification_report
+
+
+
+st.title('Machine Learning - 1st Assignment (Titanic)')
+
+st.sidebar.write("""
+This is a web app demo using python libraries such as Streamlit, Sklearn etc
+""")
+
+st.sidebar.write ("For more info, please contact:")
+
+st.sidebar.write("<a href='https://www.linkedin.com/in/ahmad-faiz-mansor-27a25a16b/'>Ahmad Faiz bin Mansor </a>", unsafe_allow_html=True)
+
+
+choice = st.sidebar.radio(
+    "Choose a dataset",   
+    ('Default', 'User-defined '),
+    index = 0
     
-    if gender=='Male':
-        details=np.array([[np.float(height),np.float(weight),0.0,1.0]])
-    elif gender=='Female':
-        details=np.array([[np.float(height),np.float(weight),1.0,0.0]])
-    
-    y_pred = grid_cv.predict(scaler.transform(details))
-    return (y_pred[0])
-    
-#Live predictor
-your_details = ['Male',175,80]
-print(lp(your_details))
+)
+
+st.write(f"## You Have Selected <font color='Aquamarine'>{choice}</font> Dataset", unsafe_allow_html=True)
+
+def get_default_dataset(name):
+    data = None
+    if name == 'Iris':
+        data = datasets.load_iris()
+    elif name == 'Wine':
+        data = datasets.load_wine()
+    else:
+        data = datasets.load_breast_cancer()
+    X = data.data
+    y = data.target
+    return X, y
+
+def add_dataset_ui(choice_name):
+    X=[]
+    y=[]
+    X_names = []
+    X1 = []
+    if choice_name == 'Default':
+       dataset_name = st.sidebar.selectbox(
+            'Select Dataset',
+            ('Iris', 'Breast Cancer', 'Wine')
+        )
+       X, y = get_default_dataset (dataset_name)
+       X_names = X
+    else:
+        uploaded_file = st.sidebar.file_uploader(
+            "Upload a CSV",
+            type='csv'    )
+        
+
+        if uploaded_file!=None:
+           
+           st.write(uploaded_file)
+           data = pd.read_csv(uploaded_file)
+  
+        
+           y_name = st.sidebar.selectbox(
+                    'Select Label @ y variable',
+                    sorted(data)
+                    )
+
+           X_names = st.sidebar.multiselect(
+                     'Select Predictors @ X variables.',
+                     sorted(data),
+                     default = sorted(data)[1],
+                     help = "You may select more than one predictor"
+                     )
+
+           y = data.loc[:,y_name]
+           X = data.loc[:,X_names]
+           X1 = X.select_dtypes(include=['object'])
+        
+           X2 = X.select_dtypes(exclude=['object'])
+
+           if sorted(X1) != []:
+              X1 = X1.apply(LabelEncoder().fit_transform)
+              X = pd.concat([X2,X1],axis=1)
+
+           y = LabelEncoder().fit_transform(y)
+        else:
+           st.write(f"## <font color='Aquamarine'>Note: Please upload a CSV file to analyze the data.</font>", unsafe_allow_html=True)
+
+    return X,y, X_names, X1
+
+X, y , X_names, cat_var= add_dataset_ui (choice)
+
+
+
+
+classifier_name = st.sidebar.selectbox(
+    'Select classifier',
+    ('KNN', 'SVM', 'Random Forest')
+)
+
+test_data_ratio = st.sidebar.slider('Select testing size or ratio', 
+                                    min_value= 0.10, 
+                                    max_value = 0.50,
+                                    value=0.2)
+random_state = st.sidebar.slider('Select random state', 1, 9999,value=1234)
+
+st.write("## 1: Summary (X variables)")
+
+
+if len(X)==0:
+   st.write("<font color='Aquamarine'>Note: Predictors @ X variables have not been selected.</font>", unsafe_allow_html=True)
+else:
+   st.write('Shape of predictors @ X variables :', X.shape)
+   st.write('Summary of predictors @ X variables:', pd.DataFrame(X).describe())
+
+st.write("## 2: Summary (y variable)")
+
+if len(y)==0:
+   st.write("<font color='Aquamarine'>Note: Label @ y variable has not been selected.</font>", unsafe_allow_html=True)
+elif len(np.unique(y)) <5:
+     st.write('Number of classes:', len(np.unique(y)))
+
+else: 
+   st.write("<font color='red'>Warning: System detects an unusual number of unique classes. Please make sure that the label @ y is a categorical variable. Ignore this warning message if you are sure that the y is a categorical variable.</font>", unsafe_allow_html=True)
+   st.write('Number of classes:', len(np.unique(y)))
+
+
+def add_parameter_ui(clf_name):
+    params = dict()
+    if clf_name == 'SVM':
+        C = st.sidebar.slider('C', 0.01, 10.0,value=1.0)
+        params['C'] = C
+    elif clf_name == 'KNN':
+        K = st.sidebar.slider('K', 1, 15,value=5)
+        params['K'] = K
+    else:
+        max_depth = st.sidebar.slider('max_depth', 2, 15,value=5)
+        params['max_depth'] = max_depth
+        n_estimators = st.sidebar.slider('n_estimators', 1, 100,value=10)
+        params['n_estimators'] = n_estimators
+    return params
+
+params = add_parameter_ui(classifier_name)
+
+def get_classifier(clf_name, params):
+    clf = None
+    if clf_name == 'SVM':
+        clf = SVC(C=params['C'])
+    elif clf_name == 'KNN':
+        clf = KNeighborsClassifier(n_neighbors=params['K'])
+    else:
+        clf = clf = RandomForestClassifier(n_estimators=params['n_estimators'], 
+            max_depth=params['max_depth'], random_state=random_state)
+    return clf
+
+clf = get_classifier(classifier_name, params)
+
+
+st.write("## 3: Classification Report")
+
+if len(X)!=0 and len(y)!=0: 
+
+
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_data_ratio, random_state=random_state)
+
+  scaler = StandardScaler()
+  X_train_scaled = scaler.fit_transform(X_train)
+  X_test_scaled = scaler.transform(X_test)    
+
+  clf.fit(X_train_scaled, y_train)
+  y_pred = clf.predict(X_test_scaled)
+
+
+  st.write('Classifier:',classifier_name)
+  st.write('Classification report:')
+  report = classification_report(y_test, y_pred,output_dict=True)
+  df = pd.DataFrame(report).transpose()
+  st.write(df)
+
+else: 
+   st.write("<font color='Aquamarine'>Note: No classification report generated.</font>", unsafe_allow_html=True)
+
+
+# st.write("## 4: Principal Component Analysis Plot")
+# suitable = 1
+# if len(X_names) <2:
+#   st.write("<font color='Aquamarine'>Note: No PCA plot as it requires at least two predictors.</font>", unsafe_allow_html=True)
+#   suitable = 0
+# else:
+#     for names in X_names:
+#         if names in cat_var:
+#            st.write("<font color='Aquamarine'>Note: No PCA plot as it only supports numerical predictors.</font>", unsafe_allow_html=True)
+#            suitable = 0
+#            break
+
+# if suitable == 1:
+#    pca = PCA(2)
+#    X_projected = pca.fit_transform(X)
+
+#    x1 = X_projected[:, 0]
+#    x2 = X_projected[:, 1]
+
+#    fig = plt.figure()
+#    plt.scatter(x1, x2,
+#                c=y, alpha=0.8,
+#                cmap='viridis')
+
+#    plt.xlabel('Principal Component 1')
+#    plt.ylabel('Principal Component 2')
+#    plt.colorbar()
+#    st.pyplot(fig)
+            
+            
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
