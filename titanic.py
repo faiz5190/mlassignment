c25bc3611162f0dca3e276fa091499b085ff2431
faiz5190 <97604215+faiz5190@users.noreply.github.com>
2022-04-02 14:07:34 +0800

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import numpy as np 
 import pandas as pd
-import train.csv
+
 
 #import matplotlib.pyplot as plt
 from matplotlib import pyplot as plt
@@ -41,8 +41,8 @@ st.write(f"## You Have Selected <font color='Aquamarine'>{choice}</font> Dataset
 
 def get_default_dataset(name):
     data = None
-    if name == 'Titanic':
-        data = datasets.load_train.csv()
+    if name == 'Iris':
+        data = datasets.load_iris.csv()
     elif name == 'Wine':
         data = datasets.load_wine()
     else:
@@ -59,7 +59,7 @@ def add_dataset_ui(choice_name):
     if choice_name == 'Default':
        dataset_name = st.sidebar.selectbox(
             'Select Dataset',
-            ('Titanic', 'Breast Cancer', 'Wine')
+            ('Iris', 'Breast Cancer', 'Wine')
         )
        X, y = get_default_dataset (dataset_name)
        X_names = X
